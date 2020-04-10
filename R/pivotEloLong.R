15881cae0c68c8c16a61fecdbde30f9fdfbc7553
Sergio Olmos <s.olmos.pardo@gmail.com>
2020-04-10 18:42:35 +0200

    Fixed team_opp and home_adv variables

--- R/pivotEloLong.R
+++ R/pivotEloLong.R
@@ -8,6 +8,7 @@
 #'
 #' @examples
 pivotEloLong <- function(df) {
+
   df_long <- df %>%
     tidyr::pivot_longer(
       cols = c(tidyselect::ends_with("home"), tidyselect::ends_with("away")),
@@ -17,12 +18,43 @@ pivotEloLong <- function(df) {
   df %>%
     dplyr::select(season, game_code, team_home, team_away) %>%
     dplyr::right_join(df_long, by = c("season", "game_code")) %>%
-    dplyr::mutate(team_opp = ifelse(where == "home", team_home, team_away)) %>%
+    dplyr::mutate(
+      team_opp = ifelse(where == "home", team_away, team_home),
+      home_adv = ifelse(where == "home", home_adv, -home_adv)
+    ) %>%
     dplyr::select(-team_home, -team_away) %>%
     dplyr::select(
-      season:game_date,
+      season:round_code,
       team, team_opp,
       winner, pt_diff, elo_diff,
       dplyr::everything()
     )
 }
+
+#df <- elohistwide
+#df_long <- df %>%
+#  tidyr::pivot_longer(
+#    cols = c(tidyselect::ends_with("home"), tidyselect::ends_with("away")),
+#    names_to = c(".value", "where"),
+#    names_pattern = "(.+_?.?)_(.+)"
+#  )
+#df %>%
+#  dplyr::select(season, game_code, team_home, team_away) %>%
+#  dplyr::right_join(df_long, by = c("season", "game_code")) %>%
+#  dplyr::mutate(team_opp = ifelse(where == "home", team_home, team_away)) %>%
+#  dplyr::select(-team_home, -team_away) %>%
+#  dplyr::select(
+#    season:round_code,
+#    team, team_opp,
+#    winner, pt_diff, elo_diff,
+#    dplyr::everything()
+#  )
+#
+#library(dplyr)
+#x <- df %>%
+#  dplyr::select(season, game_code, team_home, team_away) %>%
+#  dplyr::right_join(df_long, by = c("season", "game_code"))
+#x %>%
+#  transmute(team_opp = ifelse(where == "home", team_home, team_away)) %>%
+#
+
