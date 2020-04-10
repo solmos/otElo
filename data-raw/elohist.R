6e4ce89fbd3945a896e7a4d031d0105fbdceba7e
Sergio Olmos <s.olmos.pardo@gmail.com>
2020-04-10 18:43:26 +0200

    Updated elohist data sets

--- data-raw/elohist.R
+++ data-raw/elohist.R
@@ -4,8 +4,7 @@ data("resultshist")
 
 elohistwide <- getElo(resultshist, k = 25, home_adv = 100, s = 400, carry = 0.8)
 
-elohist <- pivotEloLong(elohistwide) %>%
-  dplyr::mutate(home_adv = ifelse(where == "home", home_adv, -home_adv))
+elohist <- pivotEloLong(elohistwide)
 
 usethis::use_data(elohist, overwrite = TRUE)
 readr::write_csv(elohist, "data-raw/elohist.csv")
